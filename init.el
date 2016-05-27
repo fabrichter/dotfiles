ae270e1d403365c97d0d548f80a6c39e192e64f5
Fabian Richter <fabian.richter.97@gmail.com>
2016-05-27 10:57:03 +0200

    blub.

--- init.el
+++ init.el
@@ -2,19 +2,33 @@
 (push '("marmalade" . "http://marmalade-repo.org/packages/") package-archives)
 (push '("melpa" . "http://melpa.milkbox.net/packages/") package-archives)
 (package-initialize)
-(require 'linum)
-(set-face-attribute 'linum nil :height 110)
-(require 'evil)
-(global-set-key (kbd "C-<SPC>") 'evil-local-mode)
-(global-set-key (kbd "M-<SPC>") 'execute-extended-command)
-(require 'evil-surround)
-(global-evil-surround-mode 1)
-(require 'evil-matchit)
-(global-evil-matchit-mode 1)
-(require 'evil-easymotion)
-(evilem-default-keybindings "SPC")
-(require 'hiwin)
-(hiwin-mode 1)
+(setq use-package-always-ensure t)
+(use-package powerline
+  :config 
+  (powerline-evil-center-color-theme)
+  )
+(use-package yalinum
+  :config
+;(set-face-attribute 'yalinum-face nil :height 110)
+(add-hook 'prog-mode-hook 'yalinum-mode)
+  )
+(setq auto-mode-alist (cons '(".*\\.pl$" . prolog-mode) auto-mode-alist))
+(use-package evil
+  :config
+    (add-hook 'prog-mode-hook 'evil-local-mode)
+    (define-key evil-insert-state-map "\C- " 'evil-local-mode)
+    (define-key evil-normal-state-map "\C- " 'evil-local-mode)
+  :bind
+    (("C-<SPC>" . evil-local-mode))
+  )
+(use-package evil-surround :config (global-evil-surround-mode 1))
+(use-package evil-matchit :config (global-evil-matchit-mode 1))
+(use-package evil-easymotion :config (evilem-default-keybindings "SPC"))
+(use-package hiwin :config (hiwin-mode 1)
+  )
+(use-package session
+  :config
+  (session-initialize))
 ; don't know if neccesarry
 (set-frame-parameter (selected-frame) 'alpha '(95 85)) 
 (add-to-list 'default-frame-alist '(alpha 95 85))
@@ -23,42 +37,32 @@
     (switch-to-buffer (other-buffer (current-buffer) 1))
 )
 (global-set-key (kbd "M-o") 'switch-to-previous-buffer)
-(define-key evil-insert-state-map "\C- " 'evil-local-mode)
-(define-key evil-normal-state-map "\C- " 'evil-local-mode)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-x C-l") 'helm-locate)
-(require 'color-theme)
-(require 'color-theme-monokai)
-(color-theme-monokai)
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-(add-to-list 'company-backends 'company-latex-commands)
-(add-to-list 'company-backends 'company-ispell)
-(require 'smartparens)
-(add-hook 'after-init-hook 'smartparens-global-mode)
-(add-hook 'after-init-hook 'smartparens-strict-mode)
-(require 'helm)
-(helm-mode 1)
-(require 'helm-adaptive)
-(helm-adaptive-mode 1)
-(require 'powerline)
-(powerline-evil-center-color-theme)
+(use-package helm
+  :config
+    (helm-mode 1)
+    :bind
+    (
+     ("C-x C-f" . helm-find-files)
+     ("M-x" . helm-M-x)
+     ("C-x b" . helm-buffers-list)
+     ("C-x C-l" . helm-locate)
+     )
+  )
+(use-package color-theme-monokai :config (color-theme-monokai))
+ (use-package magit :bind ("C-x g" . magit-status))
+(use-package company :config
+  (global-company-mode) 
+  (add-to-list 'company-backends 'company-latex-commands)
+  (add-to-list 'company-backends 'company-ispell))
+;(use-package smartparens :config (smartparens-global-mode) (smartparens-strict-mode))
 (auto-save-mode)
 (setq dired-guess-shell-alist-user '(
 				     ("\\.pdf" "mupdf &")
 				     ("\\.mp4" "mpv &")
 				     ("\\.mkv" "mpv &")))
-(require 'yasnippet)
-(yas-reload-all)
-(yas-global-mode 1)
-(require 'rainbow-delimiters)
-(add-hook 'prog-mode-hook 'linum-mode)
-(add-hook 'prog-mode-hook 'evil-local-mode)
+(use-package yasnippet :config (yas-global-mode 1))
+(use-package rainbow-delimiters :config (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 (add-hook 'prog-mode-hook 'evil-surround-mode)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 ;(require 'projectile)
 ;(projectile-global-mode)
 ;(require 'helm-projectile)
@@ -73,6 +77,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(yalinum-bar-face ((t (:background "gray20" :foreground "gray85" :height 120))))
+ '(yalinum-face ((t (:background "black" :foreground "gray70" :height 120))))
  '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "PfEd" :family "Meslo LG S Regular"))))
  '(company-preview ((t (:background "black" :foreground "gold"))))
  '(company-preview-common ((t (:inherit company-preview :foreground "gold"))))
@@ -87,8 +93,8 @@
  '(async-shell-command-buffer (quote rename-buffer))
  '(company-backends
    (quote
-    (company-bbdb company-nxml company-css company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files company-dabbrev-code company-gtags company-etags company-keywords company-oddmuse company-dabbrev company-latex-commands company-ispell)))
- '(company-idle-delay 0)
+    (company-bbdb company-nxml company-css company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files company-dabbrev-code company-gtags company-etags company-keywords company-oddmuse company-dabbrev company-ispell company-latex-commands)))
+ '(company-idle-delay 0.2)
  '(display-buffer-alist
    (quote
     ((".*Async Shell Command.*" display-buffer-no-window
@@ -98,6 +104,8 @@
  '(helm-adaptive-history-length 1000)
  '(helm-adaptive-mode t nil (helm-adaptive))
  '(initial-buffer-choice (quote eshell))
+ '(ispell-complete-word-dict "/home/fabian/.emacs.d/dict")
  '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
+ '(session-use-package t nil (session))
  '(tool-bar-mode nil))
