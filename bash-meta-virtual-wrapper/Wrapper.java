3d485a4a4be959a51b75a751cb680be2e69e85a2
Fabian Richter <fabian.richter.97@gmail.com>
2016-06-02 14:44:40 +0200

    Now with cmd.

--- bash-meta-virtual-wrapper/Wrapper.java
+++ bash-meta-virtual-wrapper/Wrapper.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 public class Wrapper {
     public static void main(String[] args) throws IOException, InterruptedException {
        String exec = String.join(" ", args);
-       ProcessBuilder pb = new ProcessBuilder("bash -c \"" + exec + "\"");
+       ProcessBuilder pb = new ProcessBuilder("cmd /c \"bash -c \"" + exec + "\"\"");
        pb.inheritIO();
        Process p = pb.start();
        System.exit(p.waitFor());
